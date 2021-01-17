67ec08fe4fca939c66690cf90b13ff208ccbd028
dimitraz <dimitrazuccarelli@gmail.com>
2021-01-17 01:50:06 +0000

    Add photos blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const ListLink = props => (
+  <li>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <header className="global-header">
+        <h1 className="main-heading">
+          <Link to="/">{title}</Link>
+        </h1>
+        <ul>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/blog/">Blog</ListLink>
+          <ListLink to="/photos/">Photos</ListLink>
+        </ul>
+      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
